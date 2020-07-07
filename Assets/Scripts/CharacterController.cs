8eec5fb7ba19f549cafde03245041af59eec4fa7
Riley McGowan <1017093@student.sae.edu.au>
2020-07-07 12:03:18 +1000

    Remove Script / Clean-up
    Removed "CharacterEventManager" as it has no use. Did some general clean-up on "CharacterController"

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+//declare our document
 public class CharacterController : MonoBehaviour
 {
+    //initial variables
     public float speed;
     public Animator animator;
     public Transform playerTransform;
-    // Update is called once per frame
+    //Our Update void for refresh per frame
     void Update()
     {
-        //getting the input from the horizontal axis. times by speed. Storing in 'translation' float.
+        //getting the input from the horizontal axis. times by speed. Storing in 'translation' float
         float translationHorizontal = Input.GetAxis("Horizontal") * speed;
+        //set our animator
         animator.SetFloat("Horizontal", translationHorizontal);
         //translating the transform on this object, times vector forward, times deltatime, times the translation which is the input
         transform.Translate(Vector3.right * Time.deltaTime * translationHorizontal);
+        //flips our character to whatever direction it is walking in
         if (translationHorizontal > 0.0)
         {
             playerTransform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
