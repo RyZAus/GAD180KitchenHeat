c8df6a5c029a204a588a006cd867e5ad74d9b14b
Riley McGowan <1017093@student.sae.edu.au>
2020-07-11 16:00:01 +1000

    Cupboard sprites and animation
    Added the sprites for the cupboard open and close animation and also wired up. 
    Issue: Currently cant see cupboard once play is activated.

--- Assets/Scripts/CupboardController.cs
+++ Assets/Scripts/CupboardController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CupboardController : MonoBehaviour
 {
-public bool isOpen;
-
+    public float speed;
+    public bool isOpen;
 void Update()
     {
        void openCupboard() // Player should trigger this when opening cupboard
