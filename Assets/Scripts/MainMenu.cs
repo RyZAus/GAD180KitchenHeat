e71c7a3f814688ecc1376f96fcbad9bab2b9ba2a
Miles <1019178@student.sae.edu.au>
2020-07-12 18:44:38 +1000

    Volume Control & Final Menu Navigation
    Volume slider in options scree now has functionality. All menu button function as intended.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public GameObject mainMenu;
+    public GameObject optionMenu;
+    public GameObject tutorialMenu;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -12,7 +20,34 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("Game has been closed");
+        print("Game has been closed");
         Application.Quit();
     }
+    public void SetVolume(float volume)
+    {
+        print("The current volume is " + volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void ReturnMenu()
+    {
+        mainMenu.gameObject.SetActive(true);
+        optionMenu.gameObject.SetActive(false);
+        tutorialMenu.gameObject.SetActive(false);
+    }
+    public void OptionScreen()
+    {
+        mainMenu.gameObject.SetActive(false);
+        optionMenu.gameObject.SetActive(true);
+        tutorialMenu.gameObject.SetActive(false);
+    }
+    public void HowToPlay()
+    {
+        mainMenu.gameObject.SetActive(false);
+        optionMenu.gameObject.SetActive(false);
+        tutorialMenu.gameObject.SetActive(true);
+    }
 }
+
+
+
+
