b81645e4faee448b2591ff33ffacbf4ad9d5a978
Damien Harwood <1019243@student.sae.edu.au>
2020-07-14 17:22:48 +1000

    Added Things
    Border on side
    Box colliders
    Added in code that will eventually be responsible the cupboard opening  
    **This Code is currently not working to playtest game just comment the code out**
    A few more things that I have forgotton

--- Assets/Scripts/CupboardController.cs
+++ Assets/Scripts/CupboardController.cs
@@ -7,8 +7,18 @@ public class CupboardController : MonoBehaviour
 {
     public float speed;
     public bool isOpen;
+    public Animator animator;
+    
+
+    
 void Update()
     {
+      if (Input.GetKeyDown(KeyCode.C)) //Code is broken still needs to be fixed Animator needs to be referenced
+        {
+            animator.SetTrigger("CupboardTrigger");
+            openCupboard();
+        }
+    
        void openCupboard() // Player should trigger this when opening cupboard
        {
            bool isOpen = true;
