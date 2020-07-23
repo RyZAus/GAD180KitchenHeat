9faff39ec2fb59305ec1f8e76cc09409918f0ce6
Miles <1019178@student.sae.edu.au>
2020-07-24 00:20:53 +1000

    Title Screen Update
    added game title
    re-positioned buttons
    added animated pot & smoke sprites

---
+++ Assets/Scripts/MenuSmoke.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSmoke : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    float destroyTime = 5;
+    // Start is called before the first frame update
+    void Update()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed);
+        SmokeGo();
+    }
+
+    IEnumerator SmokeGo()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(destroyTime);
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
